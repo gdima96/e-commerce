0401aa5e94bd3549ea4475c4e7e7b468c5d0926d
Dima <d.gotonoaga@outlook.com>
2024-11-07 21:13:56 +0200

    Implementing popular produts

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "../components/Hero/Hero";
+import Popular from "../components/Popular/Popular";
 
 const Shop = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Hero />
+      <Popular />
+    </div>
+  );
 };
 
 export default Shop;
