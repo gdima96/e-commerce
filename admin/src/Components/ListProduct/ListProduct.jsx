1c2fb8fcc96a9024765ac93f69cdf93dfca26010
Dima <d.gotonoaga@outlook.com>
2024-11-26 19:05:53 +0200

    added Listed product

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,8 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ListProduct.css";
+import cross_icon from "../../assets/cross_icon.png";
 
 const ListProduct = () => {
-  return <div className="list-product"></div>;
+  const [allProducts, setAllProducts] = useState([]);
+
+  const fetchInfo = async () => {
+    await fetch("http://localhost:4000/allproducts")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllProducts(data);
+      });
+  };
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  return (
+    <div className="list-product">
+      <h1>All Products List</h1>
+      <div className="listproducts-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproducts-allproducts">
+        <hr />
+        {allProducts.map((product, index) => {
+          return (
+            <div
+              key={index}
+              className="listproducts-format-main listproduct-format"
+            >
+              <img
+                src={product.image}
+                alt=""
+                className="listproduct-product-icon"
+              />
+              <p>{product.name}</p>
+              <p>${product.old_price}</p>
+              <p>${product.new_price}</p>
+              <p>{product.category}</p>
+              <img
+                className="listproduct-remove-icon"
+                src={cross_icon}
+                alt=""
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ListProduct;
